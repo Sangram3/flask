5e307acb36001686ae33b1b0223738a393d5776f
Sangram3 <sangramjagadale2017@gmail.com>
2023-02-01 00:35:24 +0530

    last commit

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from flask import Flask, jsonify
-import os
-
+from flask import Flask
 app = Flask(__name__)
-
-
+ 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+def home():
+    return "Hello World!"
+ 
+if __name__ == "__main__":
+    app.run()
